68e69069ff22b394a0ec50cb4a995053e8cf9d3e
Unknown <pgr3master@gmail.com>
2017-07-24 17:58:12 +0100

    Initial commit

---
+++ Emoij Dictionary/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Emoij Dictionary
+//
+//  Created by Asa Bowes on 24/07/2017.
+//  Copyright © 2017 Asa Bowes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,1 @@
+Emoij Dictionary
